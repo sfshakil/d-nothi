ca5dc017279037b28a3934992d5fdf0b78f5c47b
Md. Faisal Hossain <faisalhossainiiuc@gmail.com>
2021-03-07 22:53:40 +0600

    NothiOnumodonDesignation

--- src/dNothi.Desktop/UI/Dak/NothiOnumodonLevel.Designer.cs
+++ src/dNothi.Desktop/UI/Dak/NothiOnumodonLevel.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace dNothi.Desktop.UI.Dak
 {
-    partial class NothiOnumodonRow
+    partial class NothiOnumodonLevel
     {
         /// <summary> 
         /// Required designer variable.
@@ -28,113 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.panel3 = new System.Windows.Forms.Panel();
-            this.cbxControl = new System.Windows.Forms.CheckBox();
-            this.btnDelete = new FontAwesome.Sharp.IconButton();
-            this.lbDesignation = new System.Windows.Forms.Label();
-            this.lbName = new System.Windows.Forms.Label();
-            this.btnUser = new FontAwesome.Sharp.IconButton();
-            this.panel2 = new System.Windows.Forms.Panel();
             this.btnChair = new FontAwesome.Sharp.IconButton();
             this.LeftPanel = new System.Windows.Forms.Panel();
-            this.movementStatusSliderPanel1 = new dNothi.Desktop.CustomControl.MovementStatusSliderPanel();
             this.lbLevel = new System.Windows.Forms.Label();
             this.btnKarjodibosh = new FontAwesome.Sharp.IconButton();
             this.cbxNiontron = new System.Windows.Forms.CheckBox();
             this.deleteButton = new FontAwesome.Sharp.IconButton();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.panel3.SuspendLayout();
-            this.panel2.SuspendLayout();
+            this.officerTableLayoutPanel = new System.Windows.Forms.TableLayoutPanel();
+            this.movementStatusSliderPanel1 = new dNothi.Desktop.CustomControl.MovementStatusSliderPanel();
             this.LeftPanel.SuspendLayout();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // panel3
-            // 
-            this.panel3.BackColor = System.Drawing.SystemColors.ControlLightLight;
-            this.panel3.Controls.Add(this.cbxControl);
-            this.panel3.Controls.Add(this.btnDelete);
-            this.panel3.Controls.Add(this.lbDesignation);
-            this.panel3.Controls.Add(this.lbName);
-            this.panel3.Controls.Add(this.btnUser);
-            this.panel3.Location = new System.Drawing.Point(3, 6);
-            this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(419, 66);
-            this.panel3.TabIndex = 0;
-            // 
-            // cbxControl
-            // 
-            this.cbxControl.AutoSize = true;
-            this.cbxControl.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.cbxControl.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(54)))), ((int)(((byte)(153)))), ((int)(((byte)(255)))));
-            this.cbxControl.Location = new System.Drawing.Point(9, 44);
-            this.cbxControl.Name = "cbxControl";
-            this.cbxControl.Size = new System.Drawing.Size(15, 14);
-            this.cbxControl.TabIndex = 63;
-            this.cbxControl.UseVisualStyleBackColor = true;
-            this.cbxControl.Visible = false;
-            this.cbxControl.CheckedChanged += new System.EventHandler(this.cbxControl_CheckedChanged);
-            // 
-            // btnDelete
-            // 
-            this.btnDelete.BackColor = System.Drawing.Color.Transparent;
-            this.btnDelete.FlatAppearance.BorderColor = System.Drawing.SystemColors.ControlLightLight;
-            this.btnDelete.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnDelete.ForeColor = System.Drawing.Color.Transparent;
-            this.btnDelete.IconChar = FontAwesome.Sharp.IconChar.TrashAlt;
-            this.btnDelete.IconColor = System.Drawing.Color.Red;
-            this.btnDelete.IconFont = FontAwesome.Sharp.IconFont.Auto;
-            this.btnDelete.IconSize = 18;
-            this.btnDelete.Location = new System.Drawing.Point(4, 40);
-            this.btnDelete.Name = "btnDelete";
-            this.btnDelete.Size = new System.Drawing.Size(25, 23);
-            this.btnDelete.TabIndex = 62;
-            this.btnDelete.UseVisualStyleBackColor = false;
-            // 
-            // lbDesignation
-            // 
-            this.lbDesignation.Font = new System.Drawing.Font("SolaimanLipi", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbDesignation.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(63)))), ((int)(((byte)(66)))), ((int)(((byte)(84)))));
-            this.lbDesignation.Location = new System.Drawing.Point(165, 5);
-            this.lbDesignation.Name = "lbDesignation";
-            this.lbDesignation.Size = new System.Drawing.Size(251, 43);
-            this.lbDesignation.TabIndex = 61;
-            this.lbDesignation.Text = "সল্যুশন আর্কিটেক্ট,টেকনোলজি,এসপায়ার টু ইনোভেট (এটুআই) প্রোগ্রাম";
-            // 
-            // lbName
-            // 
-            this.lbName.Font = new System.Drawing.Font("SolaimanLipi", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbName.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(63)))), ((int)(((byte)(66)))), ((int)(((byte)(84)))));
-            this.lbName.Location = new System.Drawing.Point(35, 5);
-            this.lbName.Name = "lbName";
-            this.lbName.Size = new System.Drawing.Size(120, 20);
-            this.lbName.TabIndex = 60;
-            this.lbName.Text = "মোঃ হাসানুজ্জামান";
-            // 
-            // btnUser
-            // 
-            this.btnUser.BackColor = System.Drawing.Color.Transparent;
-            this.btnUser.FlatAppearance.BorderColor = System.Drawing.SystemColors.ControlLightLight;
-            this.btnUser.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnUser.ForeColor = System.Drawing.Color.Transparent;
-            this.btnUser.IconChar = FontAwesome.Sharp.IconChar.User;
-            this.btnUser.IconColor = System.Drawing.Color.FromArgb(((int)(((byte)(181)))), ((int)(((byte)(181)))), ((int)(((byte)(195)))));
-            this.btnUser.IconFont = FontAwesome.Sharp.IconFont.Auto;
-            this.btnUser.IconSize = 24;
-            this.btnUser.Location = new System.Drawing.Point(4, 5);
-            this.btnUser.Name = "btnUser";
-            this.btnUser.Size = new System.Drawing.Size(25, 29);
-            this.btnUser.TabIndex = 58;
-            this.btnUser.UseVisualStyleBackColor = false;
-            // 
-            // panel2
-            // 
-            this.panel2.Controls.Add(this.panel3);
-            this.panel2.Location = new System.Drawing.Point(61, 54);
-            this.panel2.Margin = new System.Windows.Forms.Padding(0);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(426, 96);
-            this.panel2.TabIndex = 8;
             // 
             // btnChair
             // 
@@ -157,27 +62,20 @@
             this.LeftPanel.BackColor = System.Drawing.SystemColors.ControlLightLight;
             this.LeftPanel.Controls.Add(this.btnChair);
             this.LeftPanel.Controls.Add(this.movementStatusSliderPanel1);
+            this.LeftPanel.Dock = System.Windows.Forms.DockStyle.Left;
             this.LeftPanel.Location = new System.Drawing.Point(0, 0);
             this.LeftPanel.Name = "LeftPanel";
-            this.LeftPanel.Size = new System.Drawing.Size(58, 150);
+            this.LeftPanel.Size = new System.Drawing.Size(58, 72);
             this.LeftPanel.TabIndex = 6;
-            // 
-            // movementStatusSliderPanel1
-            // 
-            this.movementStatusSliderPanel1.BackColor = System.Drawing.SystemColors.InactiveBorder;
-            this.movementStatusSliderPanel1.Location = new System.Drawing.Point(41, 0);
-            this.movementStatusSliderPanel1.Name = "movementStatusSliderPanel1";
-            this.movementStatusSliderPanel1.Size = new System.Drawing.Size(74, 86);
-            this.movementStatusSliderPanel1.TabIndex = 0;
             // 
             // lbLevel
             // 
             this.lbLevel.AutoSize = true;
             this.lbLevel.Font = new System.Drawing.Font("SolaimanLipi", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lbLevel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(63)))), ((int)(((byte)(66)))), ((int)(((byte)(84)))));
-            this.lbLevel.Location = new System.Drawing.Point(6, 23);
+            this.lbLevel.Location = new System.Drawing.Point(6, 18);
             this.lbLevel.Name = "lbLevel";
-            this.lbLevel.Size = new System.Drawing.Size(63, 21);
+            this.lbLevel.Size = new System.Drawing.Size(55, 18);
             this.lbLevel.TabIndex = 50;
             this.lbLevel.Text = "লেভেল ১";
             // 
@@ -192,7 +90,7 @@
             this.btnKarjodibosh.IconColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(168)))), ((int)(((byte)(0)))));
             this.btnKarjodibosh.IconFont = FontAwesome.Sharp.IconFont.Auto;
             this.btnKarjodibosh.IconSize = 32;
-            this.btnKarjodibosh.Location = new System.Drawing.Point(68, 16);
+            this.btnKarjodibosh.Location = new System.Drawing.Point(68, 11);
             this.btnKarjodibosh.Name = "btnKarjodibosh";
             this.btnKarjodibosh.Size = new System.Drawing.Size(66, 35);
             this.btnKarjodibosh.TabIndex = 55;
@@ -204,9 +102,9 @@
             this.cbxNiontron.AutoSize = true;
             this.cbxNiontron.Font = new System.Drawing.Font("SolaimanLipi", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.cbxNiontron.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(54)))), ((int)(((byte)(153)))), ((int)(((byte)(255)))));
-            this.cbxNiontron.Location = new System.Drawing.Point(140, 22);
+            this.cbxNiontron.Location = new System.Drawing.Point(140, 17);
             this.cbxNiontron.Name = "cbxNiontron";
-            this.cbxNiontron.Size = new System.Drawing.Size(75, 25);
+            this.cbxNiontron.Size = new System.Drawing.Size(65, 22);
             this.cbxNiontron.TabIndex = 56;
             this.cbxNiontron.Text = "নিয়ন্ত্রিত";
             this.cbxNiontron.UseVisualStyleBackColor = true;
@@ -221,7 +119,7 @@
             this.deleteButton.IconColor = System.Drawing.Color.Red;
             this.deleteButton.IconFont = FontAwesome.Sharp.IconFont.Auto;
             this.deleteButton.IconSize = 18;
-            this.deleteButton.Location = new System.Drawing.Point(214, 23);
+            this.deleteButton.Location = new System.Drawing.Point(214, 18);
             this.deleteButton.Name = "deleteButton";
             this.deleteButton.Size = new System.Drawing.Size(25, 23);
             this.deleteButton.TabIndex = 57;
@@ -236,38 +134,51 @@
             this.panel1.Controls.Add(this.lbLevel);
             this.panel1.Location = new System.Drawing.Point(61, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(426, 54);
+            this.panel1.Size = new System.Drawing.Size(429, 42);
             this.panel1.TabIndex = 7;
             // 
-            // NothiOnumodonRow
+            // officerTableLayoutPanel
+            // 
+            this.officerTableLayoutPanel.AutoSize = true;
+            this.officerTableLayoutPanel.ColumnCount = 1;
+            this.officerTableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.officerTableLayoutPanel.Location = new System.Drawing.Point(61, 43);
+            this.officerTableLayoutPanel.Name = "officerTableLayoutPanel";
+            this.officerTableLayoutPanel.RowCount = 1;
+            this.officerTableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.officerTableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.officerTableLayoutPanel.Size = new System.Drawing.Size(429, 26);
+            this.officerTableLayoutPanel.TabIndex = 8;
+            // 
+            // movementStatusSliderPanel1
+            // 
+            this.movementStatusSliderPanel1.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.movementStatusSliderPanel1.Location = new System.Drawing.Point(41, 0);
+            this.movementStatusSliderPanel1.Name = "movementStatusSliderPanel1";
+            this.movementStatusSliderPanel1.Size = new System.Drawing.Size(74, 86);
+            this.movementStatusSliderPanel1.TabIndex = 0;
+            // 
+            // NothiOnumodonLevel
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
             this.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.Controls.Add(this.officerTableLayoutPanel);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.LeftPanel);
-            this.Controls.Add(this.panel2);
             this.Margin = new System.Windows.Forms.Padding(0, 10, 0, 10);
-            this.Name = "NothiOnumodonRow";
-            this.Size = new System.Drawing.Size(487, 150);
-            this.panel3.ResumeLayout(false);
-            this.panel3.PerformLayout();
-            this.panel2.ResumeLayout(false);
+            this.Name = "NothiOnumodonLevel";
+            this.Size = new System.Drawing.Size(493, 72);
             this.LeftPanel.ResumeLayout(false);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Panel panel3;
-        private FontAwesome.Sharp.IconButton btnDelete;
-        private System.Windows.Forms.Label lbDesignation;
-        private System.Windows.Forms.Label lbName;
-        private FontAwesome.Sharp.IconButton btnUser;
-        private System.Windows.Forms.Panel panel2;
         private CustomControl.MovementStatusSliderPanel movementStatusSliderPanel1;
         private FontAwesome.Sharp.IconButton btnChair;
         private System.Windows.Forms.Panel LeftPanel;
@@ -276,6 +187,6 @@
         private System.Windows.Forms.CheckBox cbxNiontron;
         private FontAwesome.Sharp.IconButton deleteButton;
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.CheckBox cbxControl;
+        private System.Windows.Forms.TableLayoutPanel officerTableLayoutPanel;
     }
 }
--- src/dNothi.Desktop/UI/Dashboard.Designer.cs
+++ src/dNothi.Desktop/UI/Dashboard.Designer.cs
@@ -70,6 +70,7 @@
             this.selectAllCheckBox = new System.Windows.Forms.CheckBox();
             this.label21 = new System.Windows.Forms.Label();
             this.detailsDakSearcPanel = new System.Windows.Forms.Panel();
+            this.customDatePicker = new dNothi.Desktop.UI.ManuelUserControl.DakCustomDatePickerUserControl();
             this.searchTopTableLayoutPanel = new System.Windows.Forms.TableLayoutPanel();
             this.datePickerTableLayoutPanel = new System.Windows.Forms.TableLayoutPanel();
             this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
@@ -87,6 +88,8 @@
             this.searchDakSecurityComboBox = new System.Windows.Forms.ComboBox();
             this.searchDakPriorityComboBox = new System.Windows.Forms.ComboBox();
             this.searchDakTypeComboBox = new System.Windows.Forms.ComboBox();
+            this.searchOfficeDetailSearch = new dNothi.Desktop.UI.ManuelUserControl.SearchComboBox();
+            this.officerSearchList = new dNothi.Desktop.UI.ManuelUserControl.SearchComboBox();
             this.label11 = new System.Windows.Forms.Label();
             this.panel5 = new System.Windows.Forms.Panel();
             this.label8 = new System.Windows.Forms.Label();
@@ -177,9 +180,6 @@
             this.headerTableLayoutPanel = new System.Windows.Forms.TableLayoutPanel();
             this.footerTableLayoutPanel = new System.Windows.Forms.TableLayoutPanel();
             this.designationDetailsPanel = new dNothi.Desktop.designationSelect();
-            this.customDatePicker = new dNothi.Desktop.UI.ManuelUserControl.DakCustomDatePickerUserControl();
-            this.searchOfficeDetailSearch = new dNothi.Desktop.UI.ManuelUserControl.SearchComboBox();
-            this.officerSearchList = new dNothi.Desktop.UI.ManuelUserControl.SearchComboBox();
             this.panel4.SuspendLayout();
             this.profilePanel.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.userPictureBox)).BeginInit();
@@ -871,6 +871,26 @@
             this.detailsDakSearcPanel.TabIndex = 0;
             this.detailsDakSearcPanel.Visible = false;
             // 
+            // customDatePicker
+            // 
+            this.customDatePicker._date = null;
+            this.customDatePicker.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.customDatePicker.AutoSize = true;
+            this.customDatePicker.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.customDatePicker.BackColor = System.Drawing.Color.White;
+            this.customDatePicker.dateFrom = new System.DateTime(((long)(0)));
+            this.customDatePicker.dateTo = new System.DateTime(((long)(0)));
+            this.customDatePicker.Location = new System.Drawing.Point(721, 96);
+            this.customDatePicker.Margin = new System.Windows.Forms.Padding(0);
+            this.customDatePicker.Name = "customDatePicker";
+            this.customDatePicker.Size = new System.Drawing.Size(152, 230);
+            this.customDatePicker.TabIndex = 57;
+            this.customDatePicker.Visible = false;
+            this.customDatePicker.OptionClick += new System.EventHandler(this.customDatePicker_OptionClick);
+            this.customDatePicker.Click += new System.EventHandler(this.customDatePicker_Click);
+            this.customDatePicker.Paint += new System.Windows.Forms.PaintEventHandler(this.dakModulePanel_Paint);
+            // 
             // searchTopTableLayoutPanel
             // 
             this.searchTopTableLayoutPanel.ColumnCount = 3;
@@ -1130,6 +1150,36 @@
             this.searchDakTypeComboBox.Name = "searchDakTypeComboBox";
             this.searchDakTypeComboBox.Size = new System.Drawing.Size(209, 26);
             this.searchDakTypeComboBox.TabIndex = 4;
+            // 
+            // searchOfficeDetailSearch
+            // 
+            this.searchOfficeDetailSearch.AutoSize = true;
+            this.searchOfficeDetailSearch.BackColor = System.Drawing.Color.White;
+            this.searchOfficeDetailSearch.Font = new System.Drawing.Font("SolaimanLipi", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.searchOfficeDetailSearch.isListShown = false;
+            this.searchOfficeDetailSearch.itemList = null;
+            this.searchOfficeDetailSearch.Location = new System.Drawing.Point(560, 197);
+            this.searchOfficeDetailSearch.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.searchOfficeDetailSearch.MinimumSize = new System.Drawing.Size(140, 0);
+            this.searchOfficeDetailSearch.Name = "searchOfficeDetailSearch";
+            this.searchOfficeDetailSearch.searchButtonText = "অফিস খুঁজুন";
+            this.searchOfficeDetailSearch.Size = new System.Drawing.Size(534, 44);
+            this.searchOfficeDetailSearch.TabIndex = 53;
+            // 
+            // officerSearchList
+            // 
+            this.officerSearchList.AutoSize = true;
+            this.officerSearchList.BackColor = System.Drawing.Color.White;
+            this.officerSearchList.Font = new System.Drawing.Font("SolaimanLipi", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.officerSearchList.isListShown = false;
+            this.officerSearchList.itemList = null;
+            this.officerSearchList.Location = new System.Drawing.Point(21, 197);
+            this.officerSearchList.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.officerSearchList.MinimumSize = new System.Drawing.Size(140, 0);
+            this.officerSearchList.Name = "officerSearchList";
+            this.officerSearchList.searchButtonText = "নাম/পদবী দিয়ে খুঁজুন";
+            this.officerSearchList.Size = new System.Drawing.Size(516, 44);
+            this.officerSearchList.TabIndex = 52;
             // 
             // label11
             // 
@@ -2661,56 +2711,6 @@
             this.designationDetailsPanel.TabIndex = 51;
             this.designationDetailsPanel.Visible = false;
             this.designationDetailsPanel.LogoutButtonClick += new System.EventHandler(this.designationDetailsPanel_LogoutButtonClick);
-            // 
-            // customDatePicker
-            // 
-            this.customDatePicker._date = null;
-            this.customDatePicker.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.customDatePicker.AutoSize = true;
-            this.customDatePicker.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.customDatePicker.BackColor = System.Drawing.Color.White;
-            this.customDatePicker.dateFrom = new System.DateTime(((long)(0)));
-            this.customDatePicker.dateTo = new System.DateTime(((long)(0)));
-            this.customDatePicker.Location = new System.Drawing.Point(721, 96);
-            this.customDatePicker.Margin = new System.Windows.Forms.Padding(0);
-            this.customDatePicker.Name = "customDatePicker";
-            this.customDatePicker.Size = new System.Drawing.Size(152, 230);
-            this.customDatePicker.TabIndex = 57;
-            this.customDatePicker.Visible = false;
-            this.customDatePicker.OptionClick += new System.EventHandler(this.customDatePicker_OptionClick);
-            this.customDatePicker.Click += new System.EventHandler(this.customDatePicker_Click);
-            this.customDatePicker.Paint += new System.Windows.Forms.PaintEventHandler(this.dakModulePanel_Paint);
-            // 
-            // searchOfficeDetailSearch
-            // 
-            this.searchOfficeDetailSearch.AutoSize = true;
-            this.searchOfficeDetailSearch.BackColor = System.Drawing.Color.White;
-            this.searchOfficeDetailSearch.Font = new System.Drawing.Font("SolaimanLipi", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.searchOfficeDetailSearch.isListShown = false;
-            this.searchOfficeDetailSearch.itemList = null;
-            this.searchOfficeDetailSearch.Location = new System.Drawing.Point(560, 197);
-            this.searchOfficeDetailSearch.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.searchOfficeDetailSearch.MinimumSize = new System.Drawing.Size(140, 0);
-            this.searchOfficeDetailSearch.Name = "searchOfficeDetailSearch";
-            this.searchOfficeDetailSearch.searchButtonText = "অফিস খুঁজুন";
-            this.searchOfficeDetailSearch.Size = new System.Drawing.Size(534, 44);
-            this.searchOfficeDetailSearch.TabIndex = 53;
-            // 
-            // officerSearchList
-            // 
-            this.officerSearchList.AutoSize = true;
-            this.officerSearchList.BackColor = System.Drawing.Color.White;
-            this.officerSearchList.Font = new System.Drawing.Font("SolaimanLipi", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.officerSearchList.isListShown = false;
-            this.officerSearchList.itemList = null;
-            this.officerSearchList.Location = new System.Drawing.Point(21, 197);
-            this.officerSearchList.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.officerSearchList.MinimumSize = new System.Drawing.Size(140, 0);
-            this.officerSearchList.Name = "officerSearchList";
-            this.officerSearchList.searchButtonText = "নাম/পদবী দিয়ে খুঁজুন";
-            this.officerSearchList.Size = new System.Drawing.Size(516, 44);
-            this.officerSearchList.TabIndex = 52;
             // 
             // Dashboard
             // 
