cdaea4f7abfc233508f080bef9d6db7169044555
faisal_iiuc <faisalhossaiuniiuc@gmail.com>
2021-07-15 10:15:02 +0600

    Designation Seal- Search + Other Office

--- src/dNothi.Services/DakServices/IDesignationSealService.cs
+++ src/dNothi.Services/DakServices/IDesignationSealService.cs
@@ -1,4 +1,5 @@
-﻿using dNothi.JsonParser.Entity.Dak;
+﻿using dNothi.JsonParser.Entity;
+using dNothi.JsonParser.Entity.Dak;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,6 +12,7 @@ namespace dNothi.Services.DakServices
     {
         AllDesignationSealListResponse GetAllDesignationSeal(DakUserParam dakListUserParam, int office_id);
         OfficeListResponse GetAllOffice(DakUserParam dakListUserParam);
+        List<LocalOfficesResponse> GetAllLocalOffice();
         DesignationSealListResponse GetOfficerAddedSealList(DakUserParam dakListUserParam);
     }
 }
