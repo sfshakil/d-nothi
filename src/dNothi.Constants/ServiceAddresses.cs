0ad6dde1929f5c1beb1db74ca058f3212db57632
Pulak Deb <pulak_cse@yahoo.com>
2021-07-28 12:27:35 +0600

    serviceAddress update

--- src/dNothi.Constants/ServiceAddresses.cs
+++ src/dNothi.Constants/ServiceAddresses.cs
@@ -9,7 +9,7 @@ namespace dNothi.Constants
     public class DefaultAPIConfiguration
     {
 
-       // public const string DefaultAPIDomainAddress = "https://dev.nothibs.tappware.com";// dev server
+        //public const string DefaultAPIDomainAddress = "https://dev.nothibs.tappware.com";// dev server
         public const string DefaultAPIDomainAddress = "https://api-bes.nothi.gov.bd";// Test Server
         public const string DakNothiCountEndPoint = "/api/module/pending";
         public const string PermittedPotroEndPoint = "/api/potro/permitted";
