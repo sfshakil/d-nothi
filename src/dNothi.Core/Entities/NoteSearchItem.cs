51df8b9ed332ff6132266d3465f9468f4c660be6
Shakil Ahmed Reza <sfshakilahmed@outlook.com>
2021-09-22 12:34:00 +0600

    নোট খুঁজুন বাটন কাজ করে না।(offline) Done

---
+++ src/dNothi.Core/Entities/NoteSearchItem.cs
@@ -0,0 +1,26 @@
+﻿using dNothi.Core.Shared;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dNothi.Core.Entities
+{
+    public class NoteSearchItem : BaseEntity
+    {
+
+
+        public long nothi_id { get; set; }
+        public string note_category { get; set; }
+        public string note_subject { get; set; }
+        public string officer_designation_id { get; set; }
+
+        [MaxLength]
+        public string jsonResponse { get; set; }
+
+        public int designation_id { get; set; }
+        public int office_id { get; set; }
+    }
+}
